912586f1cbb2470fbbc5a667cad0d239f6105cfa
Heitor Adão Jr <heitor.adao@gmail.com>
2021-03-21 15:18:20 -0300

    adicionado modelo promotions, +rotas, +view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-root to: 'home#index'
+root 'home#index'
+
+resources :promotions, only: [:index]
+
 end
