4a6ad6ed63ac8b57c44401bb1dc8901a2f9ed092
Heitor Adão Jr <heitor.adao@gmail.com>
2021-03-21 13:33:19 -0300

    home_test muda de pasta

---
+++ test/system/promotion_test.rb
@@ -0,0 +1,25 @@
+require 'application_system_test_case'
+
+class PromotionTest < ApplicationSystemTestCase
+  test 'view promotions' do
+    # arrange
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+    Promotion.create!(name: 'Cyber Monday', coupon_quantity: 100,
+                      description: 'Promoção de Cyber Monday',
+                      code: 'CYBER15', discount_rate: 15,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+
+    assert_text 'Natal'
+    assert_text 'Promoção de Natal'
+    assert_text '10,00%'
+    assert_text 'Cyber Monday'
+    assert_text 'Promoção de Cyber Monday'
+    assert_text '15,00%'
+  end
+end
+
