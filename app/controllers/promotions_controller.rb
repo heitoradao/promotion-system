00f5fbafb01c4dbf34e4d07afa28acd017915ac6
Heitor Adão Jr <heitor.adao@gmail.com>
2021-03-22 15:02:07 -0300

    adicionado action generate coupons, v1

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -6,4 +6,15 @@ class PromotionsController < ApplicationController
   def show
     @promotion = Promotion.find(params[:id])
   end
+
+  def generate_coupons
+    @promotion = Promotion.find(params[:id])
+
+    (1..@promotion.coupon_quantity).each do |number|
+      Coupon.create!(code: "#{@promotion.code}-#{'%04d' % number}", promotion: @promotion)
+
+      flash[:notice] = 'Cupons gerados com sucesso'
+      redirect_to @promotion
+    end
+  end
 end
