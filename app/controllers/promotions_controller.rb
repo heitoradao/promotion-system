30972296827322cf02cbfba3f6f34c83c022d327
Heitor Adão Jr <heitor.adao@gmail.com>
2021-03-24 04:33:43 -0300

    adicionadas 2 actions para PromotionController

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,6 +1,6 @@
 class PromotionsController < ApplicationController
 
-  before_action :set_promotion, only: %i[show generate_coupons edit]
+  before_action :set_promotion, only: %i[show generate_coupons edit destroy update]
 
   def index
     @promotions = Promotion.all
@@ -29,6 +29,28 @@ class PromotionsController < ApplicationController
   end
 
   def edit
+  end
+
+  # PATCH/PUT /promotions/1 or /promotions/1.json
+  def update
+    respond_to do |format|
+      if @promotion.update(promotion_params)
+        format.html { redirect_to @promotion, notice: "Study item was successfully updated." }
+        format.json { render :show, status: :ok, location: @promotion }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @promotion.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /promotions/1 or /promotions/1.json
+  def destroy
+    @promotion.destroy
+    respond_to do |format|
+      format.html { redirect_to promotions_url, notice: "Promotion was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
