dd15925401d4521ee0b89b0393038b9334bd599f
Heitor Adão Jr <heitor.adao@gmail.com>
2021-03-29 23:20:20 -0300

    refatoração nos testes

--- test/models/promotion_test.rb
+++ test/models/promotion_test.rb
@@ -13,20 +13,29 @@ class PromotionTest < ActiveSupport::TestCase
     assert_includes promotion.errors[:expiration_date], 'não pode ficar em branco'
   end
 
+
   test 'code must be uniq' do
-    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
-                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
-                      expiration_date: '22/12/2033')
+    # precisa carregar o seed pra esse teste ser válido
+    Promotion.create!(           name: 'asdfqwer',
+                          description: 'Promoção de asdfqwer',
+                                 code: 'NATAL10',
+                        discount_rate: 123,
+                      coupon_quantity: 456,
+                      expiration_date: '22/01/2333'         )
     promotion = Promotion.new(code: 'NATAL10')
 
     refute promotion.valid?
     assert_includes promotion.errors[:code], 'já está em uso'
   end
 
+
   test 'name must be uniq' do
-    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
-                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
-                      expiration_date: '22/12/2033')
+    Promotion.create!(           name: 'Natal',
+                          description: 'Promoção de asdfasdfa',
+                                 code: 'sdfasdfasdf',
+                        discount_rate: 10,
+                      coupon_quantity: 100,
+                      expiration_date: '22/12/2033'        )
     promotion = Promotion.new(name: 'Natal')
 
     refute promotion.valid?
