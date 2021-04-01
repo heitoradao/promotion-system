96b549f882b8821ca92dfc86ebac5e32f19030a8
Heitor Adão Jr <heitor.adao@gmail.com>
2021-04-01 11:16:33 -0300

    adicionando mais testes ao user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test 'user can be instantiated' do
+    u = User.new
+    refute u.nil?
+  end
+
+  test 'empty user can not be valid' do
+    u = User.new
+    refute u.valid?
+  end
+
+  test 'this test can generate a valid user' do
+    u = create_a_valid_user
+    byebug
+    assert u.valid?
+  end
+
+  test 'user must have an email valid to be valid' do
+    u = create_a_valid_user
+    u.email = 'this is an invalid email.com.br'
+    refute u.valid?
+  end
+
+
+
+  private
+
+  def create_a_valid_user
+    User.create!(email: 'valid@iugu.com.br',
+                 password: '123456')
+  end
 end
