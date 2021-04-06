df561c3814ed0877e633c99148de264be9adfb7e
Heitor Adão Jr <heitor.adao@gmail.com>
2021-04-06 07:59:40 -0300

    adicionada rota para queimar coupon via api

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :coupons, only: [:show] do
-      end
+      resources :coupons, only: [:show, :destroy]
     end
   end
 end
