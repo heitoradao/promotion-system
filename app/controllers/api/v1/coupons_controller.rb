aecb7a39b859ba2049106fbf424c23ec3546d502
Heitor Adão Jr <heitor.adao@gmail.com>
2021-04-05 23:47:44 -0300

    nome do parametro mudou de id para code

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CouponsController < Api::V1::ApiController
   def show
-    @coupon = Coupon.find_by!(code: params[:id])
+    @coupon = Coupon.find_by!(code: params[:code])
     render json: @coupon
   rescue ActiveRecord::RcordNotFound
     head 404
